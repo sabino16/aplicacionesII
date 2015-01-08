e9fa1d141b7bff4e4452ce1c9061290f23a048fc
sabino16 <sabino16barcelona@gmail.com>
2015-01-07 19:01:01 -0500

    Avance de Reportes (Lista General de Carrera)

--- AplicacionWeb2SAAP/src/com/aplicaciones/controlador/ListaGeneralCarreraControlador.java
+++ AplicacionWeb2SAAP/src/com/aplicaciones/controlador/ListaGeneralCarreraControlador.java
@@ -10,6 +10,7 @@ import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zul.Center;
+import org.zkoss.zul.Datebox;
 import org.zkoss.zul.Doublebox;
 import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.Listbox;
@@ -37,9 +38,9 @@ public class ListaGeneralCarreraControlador extends GenericForwardComposer<Compo
 			
 		@Wire
 			Listbox lista_GeneralCarrera;
-			Textbox textbox_usuario, textbox_tarifa, textbox_origen, textbox_destino;
+			Textbox textbox_usuario, textbox_tarifa, textbox_origen, textbox_destino, textbox_tiempo;
 			Doublebox textbox_precio, textbox_km_recorridos;
-			Calendar calendar_fecha;
+			Datebox calendar_fecha;
 			Window WinListaUsuarios;
 			Center centro;
 			
@@ -77,6 +78,112 @@ public class ListaGeneralCarreraControlador extends GenericForwardComposer<Compo
 				}else{
 					alert("lista de libros no encontrada");
 				}
-				
 		}
+			
+			public void filtrar()
+			{
+				gh = new GeneralHome();
+				em = gh.initEntityManager();
+				ch = new CarreraHome();
+				ch.setEntityManager(em);
+				em.getTransaction().begin();
+
+				if((textbox_precio.getValue()!=null)&&(textbox_km_recorridos.getValue()==null)
+						&&(calendar_fecha.getValue()==null)){
+					ArrayList<Carrera> lista=(ArrayList<Carrera>) ch.filtarcamposStringPrecioHQL(textbox_usuario.getValue().toString(),
+							textbox_tarifa.getValue().toString(), textbox_origen.getValue().toString(),
+							textbox_destino.getValue().toString(), textbox_tiempo.getValue().toString(),
+							textbox_precio.getValue());
+					if(lista!=null){
+					ListModelList<Carrera> listmodel = new ListModelList<Carrera>(lista);
+					lista_GeneralCarrera.setModel(listmodel);
+					//REFRESCAR LISTA
+					lista_GeneralCarrera.renderAll();
+					}
+					else{
+						alert("lista de libros no encontrada");
+					}
+				}else
+				{
+					if((textbox_precio.getValue()==null)&&(textbox_km_recorridos.getValue()!=null)
+							&&(calendar_fecha.getValue()==null)){
+						ArrayList<Carrera> lista=(ArrayList<Carrera>) ch.filtarcamposStringKmHQL(textbox_usuario.getValue().toString(),
+								textbox_tarifa.getValue().toString(), textbox_origen.getValue().toString(),
+								textbox_destino.getValue().toString(), textbox_tiempo.getValue().toString(),
+								textbox_km_recorridos.getValue());
+						if(lista!=null){
+						ListModelList<Carrera> listmodel = new ListModelList<Carrera>(lista);
+						lista_GeneralCarrera.setModel(listmodel);
+						//REFRESCAR LISTA
+						lista_GeneralCarrera.renderAll();
+						}
+						else{
+							alert("lista de libros no encontrada");
+						}
+					}else
+					{
+						if((textbox_precio.getValue()==null)&&(textbox_km_recorridos.getValue()==null)
+								&&(calendar_fecha.getValue()!=null)){
+							ArrayList<Carrera> lista=(ArrayList<Carrera>) ch.filtarcamposStringFechaHQL(textbox_usuario.getValue().toString(),
+									textbox_tarifa.getValue().toString(), textbox_origen.getValue().toString(),
+									textbox_destino.getValue().toString(), textbox_tiempo.getValue().toString(),
+									calendar_fecha.getValue());
+							if(lista!=null){
+							ListModelList<Carrera> listmodel = new ListModelList<Carrera>(lista);
+							lista_GeneralCarrera.setModel(listmodel);
+							//REFRESCAR LISTA
+							lista_GeneralCarrera.renderAll();
+							}
+							else{
+								alert("lista de libros no encontrada");
+							}
+						}else{
+							alert("falta");
+						}
+					}
+				}
+				
+				
+				em.getTransaction().commit();
+				gh.closeEntityManager(em);
+				
+			}
+			
+			public void onChange$textbox_usuario(){
+				alert("fecha: " + calendar_fecha.getValue());
+				alert("usuario: " + textbox_usuario.getValue().toString());	
+				alert("tarifa: " + textbox_tarifa.getValue().toString());
+				alert("precio: " + textbox_precio.getValue());
+				alert("origen: " + textbox_origen.getValue().toString());
+				alert("destino: " + textbox_destino.getValue().toString());
+				alert("km: " + textbox_km_recorridos.getValue());
+			}
+			
+			public void onChange$textbox_tarifa(){
+			
+			}
+			
+			public void onChange$textbox_precio(){
+				filtrar();
+			}
+			
+			public void onChange$textbox_origen(){
+				
+			}
+			
+			public void onChange$textbox_destino(){
+				
+			}
+			
+			public void onChange$textbox_tiempo(){
+				
+			}
+			
+			public void onChange$textbox_km_recorridos(){
+				filtrar();
+			}
+			
+			public void onChange$calendar_fecha(){
+				filtrar();
+			}
 }
--- AplicacionWeb2SAAP/src/com/aplicaciones/modelo/CarreraHome.java
+++ AplicacionWeb2SAAP/src/com/aplicaciones/modelo/CarreraHome.java
@@ -2,6 +2,7 @@ package com.aplicaciones.modelo;
 
 // Generated 06/01/2015 03:56:21 PM by Hibernate Tools 3.4.0.CR1
 
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.Stateless;
@@ -161,7 +162,232 @@ public class CarreraHome {
 	}
 
 
+	public List<Carrera> filtarcamposStringHQL(String usuario , String tarifa, String origen, String destino, String tiempo){
+		try{
+		List<Carrera> lista;
+		String sql = "select c from Carrera as c where c.estado = '1' and (c.usuario.user like :criterio1 and " +
+				"c.tarifa.tipoTarifa like :criterio2 and c.origen like :criterio3 and c.destino like :criterio4 and " +
+				"c.tiempo like :criterio5 )";
+		lista = getEntityManager().createQuery(sql).setParameter("criterio1","%" + usuario + "%")
+													.setParameter("criterio2", "%" + tarifa + "%")
+													.setParameter("criterio3", "%" + origen + "%")
+													.setParameter("criterio4", "%" + destino + "%")
+													.setParameter("criterio5", "%" + tiempo + "%")
+													.getResultList();
+		
+		return lista;
+		}catch(RuntimeException re){
+			log.error("error en filtrar por usuario, tarifa, origen, destino y tiempo" +re.getMessage());
+			return null;
+		}
+	}
 
+	/*
+	public List<Carrera> filtarcamposDoubleKmHQL(Double km_recorridos){
+		try{
+		List<Carrera> lista;
+		String sql = "select c from Carrera as c where c.estado = '1' and (c.km_recorridos = :criterio1)";
+		lista = getEntityManager().createQuery(sql).setParameter("criterio1", km_recorridos).getResultList();
+		
+		return lista;
+		}catch(RuntimeException re){
+			log.error("error en filtrar por km CarreraHome" +re.getMessage());
+			return null;
+		}
+	}
 	
+	public List<Carrera> filtarcamposDoublePrecioHQL(Double precio){
+		try{
+		List<Carrera> lista;
+		String sql = "select c from Carrera as c where c.estado = '1' and (c.precio = :criterio1)";
+		lista = getEntityManager().createQuery(sql).setParameter("criterio1", precio).getResultList();
+		
+		return lista;
+		}catch(RuntimeException re){
+			log.error("error en filtrar por precio CarreraHome" +re.getMessage());
+			return null;
+		}
+	}
 	
+	public List<Carrera> filtarcamposFechaHQL(Date fecha){
+		try{
+		List<Carrera> lista;
+		String sql = "select c from Carrera as c where c.estado = '1' and (c.fecha between" +
+				" :criterio1 and :criterio2)";
+		lista = getEntityManager().createQuery(sql).setParameter("criterio1", fecha)
+				.setParameter("criterio2", fecha).getResultList();
+		
+		return lista;
+		}catch(RuntimeException re){
+			log.error("error en filtrar por fecha CarreraHome" +re.getMessage());
+			return null;
+		}
+	}*/
+	
+	public List<Carrera> filtarcamposStringKmHQL(String usuario , String tarifa, String origen, String destino, 
+			String tiempo, Double km){
+		try{
+		List<Carrera> lista;
+		String sql = "select c from Carrera as c where c.estado = '1' and (c.usuario.user like :criterio1 and " +
+				"c.tarifa.tipoTarifa like :criterio2 and c.origen like :criterio3 and c.destino like :criterio4 and " +
+				"c.tiempo like :criterio5 and c.kmRecorridos = :criterio6)";
+		lista = getEntityManager().createQuery(sql).setParameter("criterio1","%" + usuario + "%")
+													.setParameter("criterio2", "%" + tarifa + "%")
+													.setParameter("criterio3", "%" + origen + "%")
+													.setParameter("criterio4", "%" + destino + "%")
+													.setParameter("criterio5", "%" + tiempo + "%")
+													.setParameter("criterio6",km)
+													.getResultList();
+		
+		return lista;
+		}catch(RuntimeException re){
+			log.error("error en filtrar por usuario, tarifa, origen, destino, tiempo y km" +re.getMessage());
+			return null;
+		}
+	}
+	
+	public List<Carrera> filtarcamposStringPrecioHQL(String usuario , String tarifa, String origen, String destino, 
+			String tiempo, Double precio){
+		try{
+		List<Carrera> lista;
+		String sql = "select c from Carrera as c where c.estado = '1' and (c.usuario.user like :criterio1 and " +
+				"c.tarifa.tipoTarifa like :criterio2 and c.origen like :criterio3 and c.destino like :criterio4 and " +
+				"c.tiempo like :criterio5 and c.precio = :criterio6)";
+		lista = getEntityManager().createQuery(sql).setParameter("criterio1","%" + usuario + "%")
+													.setParameter("criterio2", "%" + tarifa + "%")
+													.setParameter("criterio3", "%" + origen + "%")
+													.setParameter("criterio4", "%" + destino + "%")
+													.setParameter("criterio5", "%" + tiempo + "%")
+													.setParameter("criterio6",precio)
+													.getResultList();
+		
+		return lista;
+		}catch(RuntimeException re){
+			log.error("error en filtrar por usuario, tarifa, origen, destino, tiempo y precio" +re.getMessage());
+			return null;
+		}
+	}
+	
+	public List<Carrera> filtarcamposStringFechaHQL(String usuario , String tarifa, String origen, String destino, 
+			String tiempo, Date fecha){
+		try{
+		List<Carrera> lista;
+		String sql = "select c from Carrera as c where c.estado = '1' and (c.usuario.user like :criterio1 and " +
+				"c.tarifa.tipoTarifa like :criterio2 and c.origen like :criterio3 and c.destino like :criterio4 and " +
+				"c.tiempo like :criterio5 and c.fecha between" +
+				" :criterio6 and :criterio7)";
+		lista = getEntityManager().createQuery(sql).setParameter("criterio1","%" + usuario + "%")
+													.setParameter("criterio2", "%" + tarifa + "%")
+													.setParameter("criterio3", "%" + origen + "%")
+													.setParameter("criterio4", "%" + destino + "%")
+													.setParameter("criterio5", "%" + tiempo + "%")
+													.setParameter("criterio6",fecha)
+													.setParameter("criterio7",fecha)
+													.getResultList();
+		
+		return lista;
+		}catch(RuntimeException re){
+			log.error("error en filtrar por usuario, tarifa, origen, destino, tiempo y fecha" +re.getMessage());
+			return null;
+		}
+	}
+	
+	public List<Carrera> filtarcamposStringKmPrecioHQL(String usuario , String tarifa, String origen, String destino, 
+			String tiempo, Double precio, Double Km){
+		try{
+		List<Carrera> lista;
+		String sql = "select c from Carrera as c where c.estado = '1' and (c.usuario.user like :criterio1 and " +
+				"c.tarifa.tipoTarifa like :criterio2 and c.origen like :criterio3 and c.destino like :criterio4 and " +
+				"c.tiempo like :criterio5 and c.precio = :criterio6 and c.kmRecorridos = :criterio7)";
+		lista = getEntityManager().createQuery(sql).setParameter("criterio1","%" + usuario + "%")
+													.setParameter("criterio2", "%" + tarifa + "%")
+													.setParameter("criterio3", "%" + origen + "%")
+													.setParameter("criterio4", "%" + destino + "%")
+													.setParameter("criterio5", "%" + tiempo + "%")
+													.setParameter("criterio6",precio)
+													.setParameter("criterio7",Km)
+													.getResultList();
+		
+		return lista;
+		}catch(RuntimeException re){
+			log.error("error en filtrar por usuario, tarifa, origen, destino, tiempo, precio y Km" +re.getMessage());
+			return null;
+		}
+	}
+	
+	public List<Carrera> filtarcamposStringKmFechaHQL(String usuario , String tarifa, String origen, String destino, 
+			String tiempo, Double Km, Date fecha){
+		try{
+		List<Carrera> lista;
+		String sql = "select c from Carrera as c where c.estado = '1' and (c.usuario.user like :criterio1 and " +
+				"c.tarifa.tipoTarifa like :criterio2 and c.origen like :criterio3 and c.destino like :criterio4 and " +
+				"c.tiempo like :criterio5 and c.kmRecorridos = :criterio6 and c.fecha between" +
+				" :criterio7 and :criterio8)";
+		lista = getEntityManager().createQuery(sql).setParameter("criterio1","%" + usuario + "%")
+													.setParameter("criterio2", "%" + tarifa + "%")
+													.setParameter("criterio3", "%" + origen + "%")
+													.setParameter("criterio4", "%" + destino + "%")
+													.setParameter("criterio5", "%" + tiempo + "%")
+													.setParameter("criterio6",Km)
+													.setParameter("criterio7",fecha)
+													.setParameter("criterio8",fecha)
+													.getResultList();
+		
+		return lista;
+		}catch(RuntimeException re){
+			log.error("error en filtrar por usuario, tarifa, origen, destino, tiempo, Km y fecha" +re.getMessage());
+			return null;
+		}
+	}
+	
+	public List<Carrera> filtarcamposStringPrecioFechaHQL(String usuario , String tarifa, String origen, String destino, 
+			String tiempo, Double precio, Date fecha){
+		try{
+		List<Carrera> lista;
+		String sql = "select c from Carrera as c where c.estado = '1' and (c.usuario.user like :criterio1 and " +
+				"c.tarifa.tipoTarifa like :criterio2 and c.origen like :criterio3 and c.destino like :criterio4 and " +
+				"c.tiempo like :criterio5 and c.precio = :criterio6 and c.fecha between" +
+				" :criterio7 and :criterio8)";
+		lista = getEntityManager().createQuery(sql).setParameter("criterio1","%" + usuario + "%")
+													.setParameter("criterio2", "%" + tarifa + "%")
+													.setParameter("criterio3", "%" + origen + "%")
+													.setParameter("criterio4", "%" + destino + "%")
+													.setParameter("criterio5", "%" + tiempo + "%")
+													.setParameter("criterio6",precio)
+													.setParameter("criterio7",fecha)
+													.setParameter("criterio8",fecha)
+													.getResultList();
+		
+		return lista;
+		}catch(RuntimeException re){
+			log.error("error en filtrar por usuario, tarifa, origen, destino, tiempo, precio y fecha" +re.getMessage());
+			return null;
+		}
+	}
+	
+	public List<Carrera> filtarcamposStringPrecioKmFechaHQL(String usuario , String tarifa, String origen, String destino, 
+			String tiempo, Double precio, Double Km, Date fecha){
+		try{
+		List<Carrera> lista;
+		String sql = "select c from Carrera as c where c.estado = '1' and (c.usuario.user like :criterio1 and " +
+				"c.tarifa.tipoTarifa like :criterio2 and c.origen like :criterio3 and c.destino like :criterio4 and " +
+				"c.tiempo like :criterio5 and c.kmRecorridos = :criterio6 and c.precio = :criterio7 and c.fecha between" +
+				" :criterio8 and :criterio9)";
+		lista = getEntityManager().createQuery(sql).setParameter("criterio1","%" + usuario + "%")
+													.setParameter("criterio2", "%" + tarifa + "%")
+													.setParameter("criterio3", "%" + origen + "%")
+													.setParameter("criterio4", "%" + destino + "%")
+													.setParameter("criterio5", "%" + tiempo + "%")
+													.setParameter("criterio6",Km)
+													.setParameter("criterio7",precio)
+													.setParameter("criterio8",fecha)
+													.setParameter("criterio9",fecha)
+													.getResultList();
+		
+		return lista;
+		}catch(RuntimeException re){
+			log.error("error en filtrar por usuario, tarifa, origen, destino, tiempo, precio, Km y fecha" +re.getMessage());
+			return null;
+		}
+	}
 }
